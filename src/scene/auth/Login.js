34bf586ee017d56e13783c2904e4cb7c3a6067d3
Therdsak Paradeewirai <therdsakparadeewirai@Therdsaks-MBP.true.true>
2021-02-07 11:33:17 -0800

    update

--- src/scene/auth/Login.js
+++ src/scene/auth/Login.js
@@ -13,24 +13,47 @@ import {
     KeyboardAvoidingView,
     AsyncStorage
 } from 'react-native';
-
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
+import {
+    GoogleSignin,
+    statusCodes,
+  } from '@react-native-community/google-signin';
 import axios from 'axios';
 import { Button } from 'react-native-elements';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import style from '../../styles/base'
 import { Actions } from 'react-native-router-flux'
+// GoogleSignin.configure({
+//     scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+//     webClientId: '<FROM DEVELOPER CONSOLE>', // client ID of type WEB for your server (needed to verify user ID and offline access)
+//     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+//     hostedDomain: '', // specifies a hosted domain restriction
+//     loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+//     forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+//     accountName: '', // [Android] specifies an account name on the device that should be used
+//     iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+//   });
 export default class Login extends Component {
     constructor() {
         super();
         this.state = { email: '', pass: '' }
     }
 
-    callLogin = async () => {
+
+    componentDidMount = () =>{
+        GoogleSignin.configure({
+            webClientId: '146795595892-q5e7v9jjg39gfdp5c7a9310iob9butdq.apps.googleusercontent.com', 
+            offlineAccess: true,
+          });
+    }
+
+    callLogin = async (type , email  , pass , social_id ) => {
         console.log('come in ')
         const data = {
-            "user_email": this.state.email,
-            "user_password": this.state.pass,
-            "authen_method": "local"
+            "user_email": email,
+            "user_password": pass,
+            "authen_method": type,
+            "social_id" : social_id
         }
         console.log('come in ', data)
         axios.post('https://etda.amn-corporation.com/api/backend/user/login', data)
@@ -54,6 +77,42 @@ export default class Login extends Component {
 
     };
 
+
+
+    initUser(token) {
+        fetch('https://graph.facebook.com/v2.5/me?fields=email,name,friends&access_token=' + token)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log('email : ' , json.email) 
+            // this.setState({
+            //     email : json.email
+            // }) 
+            this.callLogin('facebook' , json.email , '' , json.id)
+        })
+        .catch(() => {
+          reject('ERROR GETTING DATA FROM FACEBOOK')
+        })
+      }
+
+
+      signIn = async () => {
+        try {
+          await GoogleSignin.hasPlayServices();
+          const userInfo = await GoogleSignin.signIn();
+          this.callLogin('google' , userInfo.user.email , '' , userInfo.user.id)
+        } catch (error) {
+            console.log('error info : ' , error)
+          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            // user cancelled the login flow
+          } else if (error.code === statusCodes.IN_PROGRESS) {
+            // operation (e.g. sign in) is in progress already
+          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            // play services not available or outdated
+          } else {
+            // some other error happened
+          }
+        }
+      };
 
     callInfomation = async (token) => {
         console.log('come in ')
@@ -172,7 +231,7 @@ export default class Login extends Component {
                                 title="Login"
                                 buttonStyle={{ padding: hp('1.5%'), ...style.btnPrimary, ...style.btnRounded }}
                                 onPress={() =>
-                                    this.callLogin()
+                                    this.callLogin('local' , this.state.email , this.state.pass , '')
                                 }
                             />
                         </View>
@@ -189,12 +248,33 @@ export default class Login extends Component {
                             <Button
                                 title="Continue with Facebook"
                                 buttonStyle={{ padding: hp('1.5%'), ...style.btnFacebook, ...style.btnRounded }}
+                                onPress={() => {
+                                    LoginManager.logInWithPermissions(["public_profile", "email"])
+                                    .then((response) => {
+                                        if (response.isCancelled) {
+                                            return null
+                                        } else {
+                                            return AccessToken.getCurrentAccessToken().then(
+                                                (data) => {
+                                                    this.initUser(data.accessToken.toString())
+                                                }
+                                            )
+                                        }
+                                    }).catch((error) => {
+                                        return error
+                                    }).finally(function () {
+                                    });
+                                }
+                                }
                             />
                         </View>
                         <View style={{ marginTop: hp('2%') }}>
                             <Button
                                 title="Continue with Google"
                                 buttonStyle={{ padding: hp('1.5%'), ...style.btnGoogle, ...style.btnRounded }}
+                                onPress={() => {
+                                        this.signIn()
+                                }}
                             />
                         </View>
 
