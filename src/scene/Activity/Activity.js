b0670f18a2eb05fc498636d314bdd6711c8fcb17
apinan <iamapinan@gmail.com>
2021-02-10 02:09:57 +0700

    add poll,sureway menu

--- src/scene/Activity/Activity.js
+++ src/scene/Activity/Activity.js
@@ -66,7 +66,26 @@ export default class Activity extends Component {
                                 Lorem ipsum dolor sit amet, consetetur
                             </Text>
                         </TouchableOpacity>
+                        
+                        <TouchableOpacity style={{ ...styleScoped.warpperMenuEvent, marginTop: hp('2%') }} onPress={()=>Actions.replace('Poll')}>
+                            <Text style={{ ...styleScoped.menuName }}>
+                                Poll
+                            </Text>
+                            <Icon name="checkbox-marked-outline" color="white" style={{ ...styleScoped.menuIcon }} size={hp('6%')} />
+                            <Text style={{ ...styleScoped.description }}>
+                                Lorem ipsum dolor sit amet, consetetur
+                            </Text>
+                        </TouchableOpacity>
 
+                        <TouchableOpacity style={{ ...styleScoped.warpperMenuEvent, marginTop: hp('2%') }} onPress={()=>Actions.replace('Survey')}>
+                            <Text style={{ ...styleScoped.menuName }}>
+                                Survey
+                            </Text>
+                            <Icon name="flag" color="white" style={{ ...styleScoped.menuIcon }} size={hp('6%')} />
+                            <Text style={{ ...styleScoped.description }}>
+                                Lorem ipsum dolor sit amet, consetetur
+                            </Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
                 {this.state.user_role == "Member" ? 
