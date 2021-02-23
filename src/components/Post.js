6b6ea589d58fef7f88939ef5b0c9facef5497db7
Kraisorn086 <kraisorn.singsom@gmail.com>
2021-02-23 09:55:22 +0700

    update

--- src/components/Post.js
+++ src/components/Post.js
@@ -46,7 +46,7 @@ export default class Post extends Component {
     async UNSAFE_componentWillMount() {
         await this.getLang();
     }
-    
+
     async getLang() {
         this.setState({ isFetching: true })
         let vocap = await translate()
@@ -366,40 +366,40 @@ export default class Post extends Component {
                 </TouchableOpacity>
 
                 <View style={{
-                        justifyContent: 'flex-start',
-                        marginTop: hp('2%'),
-                        paddingTop: hp('1.5%'),
-                        flexDirection: 'row',
-                        width: wp('100%'),
-                        paddingHorizontal: hp('2%'),
-                    }}>
-                    <TouchableOpacity onPress={() => this.callPostLike(post_id)} style={{flexDirection: 'row'}}>
+                    justifyContent: 'flex-start',
+                    marginTop: hp('2%'),
+                    paddingTop: hp('1.5%'),
+                    flexDirection: 'row',
+                    width: wp('100%'),
+                    paddingHorizontal: hp('2%'),
+                }}>
+                    <TouchableOpacity onPress={() => this.callPostLike(post_id)} style={{ flexDirection: 'row' }}>
                         <Icon name="thumb-up" size={hp('2.5%')} style={{ marginRight: hp('1%'), color: '#B5B5B5' }} />
                         <Text style={{ marginRight: hp('3%'), color: '#B5B5B5' }}> {like}</Text>
                     </TouchableOpacity>
 
                     <Icon name="eye" size={hp('2.5%')} style={{ marginRight: hp('1%'), color: '#B5B5B5' }} />
-                    <Text style={{ marginRight: hp('3%'), color: '#B5B5B5' }}> {view === undefined?0:view}</Text>
+                    <Text style={{ marginRight: hp('3%'), color: '#B5B5B5' }}> {view === undefined ? 0 : view}</Text>
 
                     <TouchableOpacity onPress={() => this.sharePOST(share_url)}>
                         <Icon name="share-outline" size={hp('2.5%')} style={{ marginRight: hp('1%'), color: '#B5B5B5' }} />
                     </TouchableOpacity>
                 </View>
                 <TouchableOpacity
-                onPress={() => this.postView()}
-                style={{
-                    ...style.flex__start,
-                    marginTop: hp('2%'),
-                    paddingTop: hp('1.5%'),
-                    borderTopWidth: 1,
-                    borderTopColor: '#B5B5B5',
-                    alignItems: 'center',
-                    paddingHorizontal: hp('2%'),
-                }}>
+                    onPress={() => this.postView()}
+                    style={{
+                        ...style.flex__start,
+                        marginTop: hp('2%'),
+                        paddingTop: hp('1.5%'),
+                        borderTopWidth: 1,
+                        borderTopColor: '#B5B5B5',
+                        alignItems: 'center',
+                        paddingHorizontal: hp('2%'),
+                    }}>
                     <Icon name="comment-outline" size={hp('2.5%')} style={{ marginRight: hp('1%'), color: '#B5B5B5' }} />
                     <Text style={{ color: '#B5B5B5' }}>{comment_number}  {lng.comment}</Text>
                 </TouchableOpacity>
-           
+
                 {/* <View style={{ ...style.sectionSocial }}>
                     <TouchableOpacity style={style.flex__start} onPress={() => this.callPostLike(post_id)}>
                         <Icon name="thumb-up" size={hp('2.5%')} style={{ marginRight: hp('1%'), color: is_like ? '#4267B2' : '#B5B5B5' }} />
