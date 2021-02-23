6c616ece5032c36682c8509332b11f0975b8fe66
Kraisorn086 <kraisorn.singsom@gmail.com>
2021-02-23 10:42:09 +0700

    update poll create user role

--- src/components/Post.js
+++ src/components/Post.js
@@ -401,7 +401,7 @@ export default class Post extends Component {
                 </TouchableOpacity>
 
                 {this.renderBottomSheet()}
-                {this.renderModalReport()}
+                {/* {this.renderModalReport()} */}
             </View>
         );
     }
